ca9584790808ed2a1c1f62099923f3b1ca85e234
1gn1z <kebor327@gmail.com>
2023-04-27 18:04:01 -0600

    inicializar la ventana de Pygame

--- Life.py
+++ Life.py
@@ -9,3 +9,13 @@ import numpy as np
 BLACK = (0, 0 ,0)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
+
+# Dimensiones de la ventana del juego y velocidad actualización
+WINDOW_SIZE = (500, 500)    # píxeles
+FPS = 60                    # 60 cuadros por segundo
+
+
+# Inicializar la ventana de Pygame
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE) # la pantalla se muestra con el tamaño indicado
+pygame.display.set_caption("Life by BytePunk")
