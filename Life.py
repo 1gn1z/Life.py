4bd3b50d5e4cf626fb9e0056b48777d8c0073d00
1gn1z <kebor327@gmail.com>
2023-04-27 18:00:15 -0600

    definimos los colores

--- Life.py
+++ Life.py
@@ -1,7 +1,11 @@
 # Importamos las librerias necesarias:
 
 import pygame
-
-# "np" es un alias, para no tener que escribir numpy.funcion | es mejor np.funcion
 import numpy as np
+# "np" es un alias, para no tener que escribir numpy.funcion | es mejor np.funcion
 
+
+# Definir los colores que se usarán en el juego (valores RGB)
+BLACK = (0, 0 ,0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
